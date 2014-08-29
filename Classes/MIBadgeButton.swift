44e1694a02d4c06bc31b71e82f1594ea89b027e8
Ahmed Ibrahim <ahmedibrahim@Ahmeds-MacBook-Pro.local>
2014-08-29 07:45:17 +0300

    fix some compilation errors due to failure to convert from CGFloat to float, and some code refactoring

--- Classes/MIBadgeButton.swift
+++ Classes/MIBadgeButton.swift
@@ -31,7 +31,7 @@ class MIBadgeButton: UIButton {
         self.setupBadgeViewWithString(badgeText: "")
     }
     
-    required init(coder aDecoder: NSCoder!) {
+    required init(coder aDecoder: NSCoder) {
         calculationTextView = UITextView()
         badgeLabel = MIBadgeLabel(frame: CGRectMake(0, 0, 10, 10))
         super.init(coder: aDecoder)
@@ -50,14 +50,20 @@ class MIBadgeButton: UIButton {
     func setupBadgeViewWithString(#badgeText: String?) {
         badgeLabel.clipsToBounds = true
         badgeLabel.text = badgeText
-        var badgeSize: CGSize  = badgeLabel.sizeThatFits(CGSizeMake(320, FLT_MAX))
+        var badgeSize: CGSize  = badgeLabel.sizeThatFits(CGSize(width: 320, height: Int(FLT_MAX)))
         badgeSize.width = badgeSize.width < 20 ? 25 : badgeSize.width + 5
         
-        var vertical: Float?, horizontal: Float?
+        var vertical: Double?, horizontal: Double?
         if let badgeInset = self.badgeEdgeInsets {
-            vertical = badgeInset.top - badgeInset.bottom
-            horizontal = badgeInset.left - badgeInset.right
-            badgeLabel.frame = CGRectMake(self.bounds.size.width - 5 + horizontal!, -(badgeSize.height / 2) - 5 + vertical!, badgeSize.width, badgeSize.height)
+            vertical = Double(badgeInset.top) - Double(badgeInset.bottom)
+            horizontal = Double(badgeInset.left) - Double(badgeInset.right)
+            
+            let x = (Double(bounds.size.width) - 5 + horizontal!)
+            let y = -(Double(badgeSize.height) / 2) - 5 + vertical!
+            let width = Double(badgeSize.width)
+            let height = Double(badgeSize.height)
+            
+            badgeLabel.frame = CGRect(x: x, y: y, width: width, height: height)
         } else {
             badgeLabel.frame = CGRectMake(self.bounds.size.width - 5, -(badgeSize.height / 2) - 5, badgeSize.width, badgeSize.height)
         }
@@ -65,7 +71,7 @@ class MIBadgeButton: UIButton {
         self.setupBadgeStyle()
         self.addSubview(badgeLabel)
         
-        badgeLabel.hidden = badgeText ? false : true
+        badgeLabel.hidden = badgeText != nil ? false : true
     }
     
     func setupBadgeStyle()
