dd8f432110fe37056e769a2373fbc44eaa93fcdb
Mustafa Ibrahim <mustafaibrahim@Mustafas-MacBook-Pro.local>
2014-09-11 16:12:26 +0300

    -- add Chameleon -- change screenshot

---
+++ MIBadgeButton-Bridging-Header.h
@@ -0,0 +1,9 @@
+//
+//  MIBadgeButton-Bridging-Header.h
+//  MIBadgeButton
+//
+//  Created by Mustafa Ibrahim on 9/10/14.
+//  Copyright (c) 2014 Youxel Technology. All rights reserved.
+//
+
+#import "Chameleon.h"
